c0dd2466141acafb1f26ebe4aa346d1938f17d55
jagriti-12 <jags.jagriti12@gmail.com>
2025-12-03 14:16:29 +0530

    UI Changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,47 +23,47 @@ export default function Hero() {
                         <strong>Frontend Developer • UI/UX Designer • Data Analyst</strong>
                         <br /><br />
                         Recent Computer Science graduate from
-                        <u><i> Chandigarh University </i></u> exploring the realm of
-                        <strong> frontend development, user experience design, and data insights.</strong>
-                    </motion.p>
-                    {/* CTA Buttons */}
-                    <div className="mt-6 flex gap-3">
-                        <a
-                            href="#resume"
-                            className="px-5 py-4 rounded-2xl bg-[var(--bg-secondary)] text-[var(--text-primary)] text-m border border-gray-700 font-semibold shadow-lg hover:scale-105 transition"
-                        >
-                            View Resumes
-                        </a>
-                        <button
-                            onClick={() => window.dispatchEvent(new Event("openChat"))}
-                            className="px-6 py-3 rounded-2xl bg-[var(--bg-secondary)] text-[var(--text-primary)] font-semibold border border-gray-700 hover:scale-105 transition"
-                        >
-                            Ask Jagriti’s AI →
-                        </button>
-                    </div>
-                    {/* Skill Pills */}
-                    <div className="mt-8 flex flex-wrap gap-3">
-                        {["Next.js", "Django", "Python", "AI", "Figma", "SQL", "PowerBi"].map((skill) => (
-                            <span
-                                key={skill}
-                                className="inline-flex items-center gap-2 px-3 py-2 rounded-full bg-white/5 text-sm text-[var(--text-secondary)] backdrop-blur-sm border border-white/10"
-                            >
-                                {skill}
-                            </span>
-                        ))}
-                    </div>
+                        <i> <u>Chandigarh University</u></i> exploring the realm of
+                    <strong> frontend development, user experience design, and data insights.</strong>
+                </motion.p>
+                {/* CTA Buttons */}
+                <div className="mt-6 flex gap-3">
+                    <a
+                        href="#resume"
+                        className="px-5 py-4 rounded-2xl bg-[var(--bg-secondary)] text-[var(--text-primary)] text-m border border-gray-700 font-semibold shadow-lg hover:scale-105 transition"
+                    >
+                        View Resumes
+                    </a>
+                    <button
+                        onClick={() => window.dispatchEvent(new Event("openChat"))}
+                        className="px-6 py-3 rounded-2xl bg-[var(--bg-secondary)] text-[var(--text-primary)] font-semibold border border-gray-700 hover:scale-105 transition"
+                    >
+                        Ask Jagriti’s AI →
+                    </button>
                 </div>
-                {/* RIGHT — 3D MODEL */}
-                <div className="flex items-center justify-center">
-                    <div className="w-72 h-90 md:w-90 md:h-120 rounded-2xl overflow-hidden">
-                        <img
-                            src="/3dmodel.png"
-                            alt="Jagriti Sachdeva 3D avatar representing Frontend Developer and UI/UX Designer"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
+                {/* Skill Pills */}
+                <div className="mt-8 flex flex-wrap gap-3">
+                    {["Next.js", "Django", "Python", "AI", "Figma", "SQL", "PowerBi"].map((skill) => (
+                        <span
+                            key={skill}
+                            className="inline-flex items-center gap-2 px-3 py-2 rounded-full bg-white/5 text-sm text-[var(--text-secondary)] backdrop-blur-sm border border-white/10"
+                        >
+                            {skill}
+                        </span>
+                    ))}
                 </div>
             </div>
-        </header>
+            {/* RIGHT — 3D MODEL */}
+            <div className="flex items-center justify-center">
+                <div className="w-72 h-90 md:w-90 md:h-120 rounded-2xl overflow-hidden">
+                    <img
+                        src="/3dmodel.png"
+                        alt="Jagriti 3D avatar representing Frontend Developer and UI/UX Designer"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+        </header >
     );
 }
